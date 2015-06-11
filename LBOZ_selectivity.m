65607bf64e1732a5f916aff5d33920a6af48006a
raf <rafjaimes@gmail.com>
2015-06-11 19:44:48 -0400

    "waxis branch only"

--- LBOZ_selectivity.m
+++ LBOZ_selectivity.m
@@ -52,4 +52,5 @@ legend('Prediction','Window','Residuals')
 % SE=std(win)./(ksi.*sqrt(sum(S.^2))); % 2-norm is the Euclidean norm.
 % Test1
 % Almost done
+% This is part of the waxis branch
 SE=std(win)./(ksi*norm(S(:,1))); % ONE AT A TIME
